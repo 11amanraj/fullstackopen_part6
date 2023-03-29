a5d41b3a6eba90eda55588e6893927f65b057184
11amanraj <110328977+11amanraj@users.noreply.github.com>
2023-03-29 17:37:59 +0530

    6.3: anecdotes, step1

---
+++ redux-anecdotes/src/tests/reducer.test.js
@@ -0,0 +1,23 @@
+import deepFreeze from 'deep-freeze'
+import anecdoteReducer from '../reducers/anecdoteReducer'
+
+describe('anecdote reducer', () => {
+    const initialState = [{
+        id: 4567,
+        content: 'Testing Reducer',
+        votes: 0
+    }]
+
+    test('vote is increased', () => {
+        const id = 4567
+        const action = {
+            type: 'vote',
+            payload: { id: id }
+        }
+        const state = initialState
+
+        deepFreeze(state)
+        const newState = anecdoteReducer(state, action)
+        expect(newState.find(anecdote => anecdote.id === id).votes).toEqual(1)
+    })
+})
